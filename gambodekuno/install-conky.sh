8f76d1c469d23c38226194b439af153bae487ac2
Quentin Meeus <qmeeus@gmail.com>
2019-01-05 10:12:51 +0100

    adapt settings of bambodekuno for my personal use

--- gambodekuno/install-conky.sh
+++ gambodekuno/install-conky.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 #
-#                                       
-# MMMMMMMMMMMMMMMMMMMMMMMMMmds+.        
-# MMm----::-://////////////oymNMd+`     
-# MMd      /++                -sNMd:    
-# MMNso/`  dMM    `.::-. .-::.` .hMN:   
-# ddddMMh  dMM   :hNMNMNhNMNMNh: `NMm   
-#     NMm  dMM  .NMN/-+MMM+-/NMN` dMM   
-#     NMm  dMM  -MMm  `MMM   dMM. dMM   
-#     NMm  dMM  -MMm  `MMM   dMM. dMM   
-#     NMm  dMM  .mmd  `mmm   yMM. dMM   
-#     NMm  dMM`  ..`   ...   ydm. dMM   
-#     hMM- +MMd/-------...-:sdds  dMM   
-#     -NMm- :hNMNNNmdddddddddy/`  dMM   
-#      -dMNs-``-::::-------.``    dMM   
-#       `/dMNmy+/:-------------:/yMMM  
-#          ./ydNMMMMMMMMMMMMMMMMMMMMM  
-#             \.MMMMMMMMMMMMMMMMMMM    
-#                                      
+#
+# MMMMMMMMMMMMMMMMMMMMMMMMMmds+.
+# MMm----::-://////////////oymNMd+`
+# MMd      /++                -sNMd:
+# MMNso/`  dMM    `.::-. .-::.` .hMN:
+# ddddMMh  dMM   :hNMNMNhNMNMNh: `NMm
+#     NMm  dMM  .NMN/-+MMM+-/NMN` dMM
+#     NMm  dMM  -MMm  `MMM   dMM. dMM
+#     NMm  dMM  -MMm  `MMM   dMM. dMM
+#     NMm  dMM  .mmd  `mmm   yMM. dMM
+#     NMm  dMM`  ..`   ...   ydm. dMM
+#     hMM- +MMd/-------...-:sdds  dMM
+#     -NMm- :hNMNNNmdddddddddy/`  dMM
+#      -dMNs-``-::::-------.``    dMM
+#       `/dMNmy+/:-------------:/yMMM
+#          ./ydNMMMMMMMMMMMMMMMMMMMMM
+#             \.MMMMMMMMMMMMMMMMMMM
+#
 #
 #
 ##################################################################################################################
@@ -31,14 +31,14 @@
 # Author 	: 	Erik Dubois
 # Website 	: 	http://www.erikdubois.be
 ##################################################################################################################
-# 
+#
 # More from Erik Dubois
 #
 # Aurora Conky
 # at http://sourceforge.net/projects/auroraconkytheme/
-# Explanation on the use of this theme can be found at 
+# Explanation on the use of this theme can be found at
 # http://erikdubois.be/category/linux/aurora-conky/
-# 
+#
 # Aureola Conky
 # Collections of nice conky's with lua syntax
 # https://github.com/erikdubois/Aureola
@@ -117,14 +117,14 @@ if [ "$(ls -A ~/.config/conky)" ] ; then
 	echo "################################################################"
 	read -p "Everything in folder ~/.config/conky will be deleted. Are you sure? (y/n)?" choice
 
-	case "$choice" in 
+	case "$choice" in
  	 y|Y ) rm -r ~/.config/conky/*;;
  	 n|N ) echo "No files have been changed in folder ~/.config/conky." & echo "Script ended!" & exit;;
  	 * ) echo "Type y or n." & echo "Script ended!" & exit;;
 	esac
 
 else
-	echo "################################################################" 
+	echo "################################################################"
 	echo "Installation folder is ready and empty. Files will now be copied."
 
 fi
@@ -137,12 +137,12 @@ fi
 
 
 
-echo "################################################################" 
+echo "################################################################"
 echo "The files have been copied to ~/.config/conky."
 # the standard place conky looks for a config file
 cp -r * ~/.config/conky/
 
-echo "################################################################" 
+echo "################################################################"
 echo "Making sure conky autostarts next boot."
 # making sure conky is started at boot
 cp start-conky.desktop ~/.config/autostart/start-conky.desktop
@@ -155,7 +155,7 @@ cp start-conky.desktop ~/.config/autostart/start-conky.desktop
 
 
 
-echo "################################################################" 
+echo "################################################################"
 echo "Installing the fonts if you do not have it yet - with choice"
 
 FONT="Inconsolata"
@@ -163,31 +163,31 @@ FONT="Inconsolata"
 
 if fc-list | grep -i $FONT >/dev/null ; then
 
-	echo "################################################################" 
+	echo "################################################################"
     echo "The font is already available. Proceeding ...";
 
 else
-	echo "################################################################" 
+	echo "################################################################"
     echo "The font is not currently installed, would you like to install it now? (y/n)";
     read response
     if [[ "$response" == [yY] ]]; then
         echo "Installing the font to the ~/.fonts directory.";
         cp ~/.config/conky/fonts/* ~/.fonts
-        echo "################################################################" 
+        echo "################################################################"
         echo "Building new fonts into the cache files";
-        echo "Depending on the number of fonts, this may take a while..." 
+        echo "Depending on the number of fonts, this may take a while..."
         fc-cache -fv ~/.fonts
-		echo "################################################################" 
-		echo "Check if the cache build was successful?";    
+		echo "################################################################"
+		echo "Check if the cache build was successful?";
         if fc-list | grep -i $FONT >/dev/null; then
-            echo "################################################################" 
+            echo "################################################################"
             echo "The font was sucessfully installed!";
         else
-        	echo "################################################################" 
+        	echo "################################################################"
             echo "Something went wrong while trying to install the font.";
         fi
     else
-    	echo "################################################################" 	
+    	echo "################################################################"
         echo "Skipping the installation of the font.";
         echo "Please note that this conky configuration will not work";
         echo "correctly without the font.";
@@ -197,7 +197,7 @@ fi
 
 
 
-echo "################################################################" 
+echo "################################################################"
 echo "Installing the fonts if you do not have it yet - with choice"
 
 FONT="Feenacasual"
@@ -205,31 +205,31 @@ FONT="Feenacasual"
 
 if fc-list | grep -i $FONT >/dev/null ; then
 
-	echo "################################################################" 
+	echo "################################################################"
     echo "The font is already available. Proceeding ...";
 
 else
-	echo "################################################################" 
+	echo "################################################################"
     echo "The font is not currently installed, would you like to install it now? (y/n)";
     read response
     if [[ "$response" == [yY] ]]; then
         echo "Installing the font to the ~/.fonts directory.";
         cp ~/.config/conky/fonts/* ~/.fonts
-        echo "################################################################" 
+        echo "################################################################"
         echo "Building new fonts into the cache files";
-        echo "Depending on the number of fonts, this may take a while..." 
+        echo "Depending on the number of fonts, this may take a while..."
         fc-cache -fv ~/.fonts
-		echo "################################################################" 
-		echo "Check if the cache build was successful?";    
+		echo "################################################################"
+		echo "Check if the cache build was successful?";
         if fc-list | grep -i $FONT >/dev/null; then
-            echo "################################################################" 
+            echo "################################################################"
             echo "The font was sucessfully installed!";
         else
-        	echo "################################################################" 
+        	echo "################################################################"
             echo "Something went wrong while trying to install the font.";
         fi
     else
-    	echo "################################################################" 	
+    	echo "################################################################"
         echo "Skipping the installation of the font.";
         echo "Please note that this conky configuration will not work";
         echo "correctly without the font.";
@@ -237,7 +237,7 @@ else
 
 fi
 
-echo "################################################################" 
+echo "################################################################"
 echo "Installing the fonts if you do not have it yet - with choice"
 
 FONT="KnifeFightBallet-Regular"
@@ -245,31 +245,31 @@ FONT="KnifeFightBallet-Regular"
 
 if fc-list | grep -i $FONT >/dev/null ; then
 
-	echo "################################################################" 
+	echo "################################################################"
     echo "The font is already available. Proceeding ...";
 
 else
-	echo "################################################################" 
+	echo "################################################################"
     echo "The font is not currently installed, would you like to install it now? (y/n)";
     read response
     if [[ "$response" == [yY] ]]; then
         echo "Installing the font to the ~/.fonts directory.";
         cp ~/.config/conky/fonts/* ~/.fonts
-        echo "################################################################" 
+        echo "################################################################"
         echo "Building new fonts into the cache files";
-        echo "Depending on the number of fonts, this may take a while..." 
+        echo "Depending on the number of fonts, this may take a while..."
         fc-cache -fv ~/.fonts
-		echo "################################################################" 
-		echo "Check if the cache build was successful?";    
+		echo "################################################################"
+		echo "Check if the cache build was successful?";
         if fc-list | grep -i $FONT >/dev/null; then
-            echo "################################################################" 
+            echo "################################################################"
             echo "The font was sucessfully installed!";
         else
-        	echo "################################################################" 
+        	echo "################################################################"
             echo "Something went wrong while trying to install the font.";
         fi
     else
-    	echo "################################################################" 	
+    	echo "################################################################"
         echo "Skipping the installation of the font.";
         echo "Please note that this conky configuration will not work";
         echo "correctly without the font.";
@@ -297,7 +297,7 @@ echo "################################################################"
 
 
 
-case $DISTRO in 
+case $DISTRO in
 
 	LinuxMint|linuxmint|Ubuntu|ubuntu)
 
@@ -388,7 +388,7 @@ echo "################################################################"
 echo "Starting the conky"
 echo "################################################################"
 
-#starting the conky 
+
 conky -q -c ~/.config/conky/conky.conf
 
 echo "################################################################"
