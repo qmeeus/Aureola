89c620755d15ae08e3bab53ad3b91d596ad39448
Quentin Meeus <qmeeus@gmail.com>
2019-12-29 13:52:08 +0100

    update install script and settings

--- cherry/install-conky.sh
+++ cherry/install-conky.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 #
-#                                       
-# MMMMMMMMMMMMMMMMMMMMMMMMMmds+.        
-# MMm----::-://////////////oymNMd+`     
-# MMd      /++                -sNMd:    
-# MMNso/`  dMM    `.::-. .-::.` .hMN:   
-# ddddMMh  dMM   :hNMNMNhNMNMNh: `NMm   
-#     NMm  dMM  .NMN/-+MMM+-/NMN` dMM   
-#     NMm  dMM  -MMm  `MMM   dMM. dMM   
-#     NMm  dMM  -MMm  `MMM   dMM. dMM   
-#     NMm  dMM  .mmd  `mmm   yMM. dMM   
-#     NMm  dMM`  ..`   ...   ydm. dMM   
-#     hMM- +MMd/-------...-:sdds  dMM   
-#     -NMm- :hNMNNNmdddddddddy/`  dMM   
-#      -dMNs-``-::::-------.``    dMM   
-#       `/dMNmy+/:-------------:/yMMM  
-#          ./ydNMMMMMMMMMMMMMMMMMMMMM  
-#             \.MMMMMMMMMMMMMMMMMMM    
-#                                      
+#
+# MMMMMMMMMMMMMMMMMMMMMMMMMmds+.
+# MMm----::-://////////////oymNMd+`
+# MMd      /++                -sNMd:
+# MMNso/`  dMM    `.::-. .-::.` .hMN:
+# ddddMMh  dMM   :hNMNMNhNMNMNh: `NMm
+#     NMm  dMM  .NMN/-+MMM+-/NMN` dMM
+#     NMm  dMM  -MMm  `MMM   dMM. dMM
+#     NMm  dMM  -MMm  `MMM   dMM. dMM
+#     NMm  dMM  .mmd  `mmm   yMM. dMM
+#     NMm  dMM`  ..`   ...   ydm. dMM
+#     hMM- +MMd/-------...-:sdds  dMM
+#     -NMm- :hNMNNNmdddddddddy/`  dMM
+#      -dMNs-``-::::-------.``    dMM
+#       `/dMNmy+/:-------------:/yMMM
+#          ./ydNMMMMMMMMMMMMMMMMMMMMM
+#             \.MMMMMMMMMMMMMMMMMMM
+#
 #
 #
 ##################################################################################################################
@@ -31,14 +31,14 @@
 # Author 	: 	Erik Dubois
 # Website 	: 	http://www.erikdubois.be
 ##################################################################################################################
-# 
+#
 # More from Erik Dubois
 #
 # Aurora Conky
 # at http://sourceforge.net/projects/auroraconkytheme/
-# Explanation on the use of this theme can be found at 
+# Explanation on the use of this theme can be found at
 # http://erikdubois.be/category/linux/aurora-conky/
-# 
+#
 # Aureola Conky
 # Collections of nice conky's with lua syntax
 # https://github.com/erikdubois/Aureola
@@ -77,7 +77,6 @@
 # Lua syntax!!
 
 # killing whatever conkies are still working
-echo "################################################################"
 echo "Stopping conky's if available"
 
 
@@ -114,17 +113,15 @@ sleep 1
 
 if [ "$(ls -A ~/.config/conky)" ] ; then
 
-	echo "################################################################"
 	read -p "Everything in folder ~/.config/conky will be deleted. Are you sure? (y/n)?" choice
 
-	case "$choice" in 
+	case "$choice" in
  	 y|Y ) rm -r ~/.config/conky/*;;
  	 n|N ) echo "No files have been changed in folder ~/.config/conky." & echo "Script ended!" & exit;;
  	 * ) echo "Type y or n." & echo "Script ended!" & exit;;
 	esac
 
 else
-	echo "################################################################" 
 	echo "Installation folder is ready and empty. Files will now be copied."
 
 fi
@@ -137,12 +134,10 @@ fi
 
 
 
-echo "################################################################" 
 echo "The files have been copied to ~/.config/conky."
 # the standard place conky looks for a config file
 cp -r * ~/.config/conky/
 
-echo "################################################################" 
 echo "Making sure conky autostarts next boot."
 # making sure conky is started at boot
 cp start-conky.desktop ~/.config/autostart/start-conky.desktop
@@ -153,116 +148,63 @@ cp start-conky.desktop ~/.config/autostart/start-conky.desktop
 ########################                           F O N T S                            ##########################
 ##################################################################################################################
 
-echo "################################################################" 
-echo "Installing the fonts if you do not have it yet - with choice"
+install_font() {
 
-FONT="Feenacasual"
+  echo "################################################################"
+  echo "Installing the fonts if you do not have it yet - with choice"
 
+  FONT="$1"
 
-if fc-list | grep -i $FONT >/dev/null ; then
+  if ! [ $FONT ]; then
+    echo Missing arguments FONT
+    return
+  fi
 
-	echo "################################################################" 
+  if fc-list | grep -i $FONT >/dev/null ; then
+
+	  echo "################################################################"
     echo "The font is already available. Proceeding ...";
 
-else
-	echo "################################################################" 
+  else
+	  echo "################################################################"
     echo "The font is not currently installed, would you like to install it now? (y/n)";
     read response
     if [[ "$response" == [yY] ]]; then
         echo "Installing the font to the ~/.fonts directory.";
         cp ~/.config/conky/fonts/* ~/.fonts
-        echo "################################################################" 
+        echo "################################################################"
         echo "Building new fonts into the cache files";
-        echo "Depending on the number of fonts, this may take a while..." 
+        echo "Depending on the number of fonts, this may take a while..."
         fc-cache -fv ~/.fonts
-		echo "################################################################" 
-		echo "Check if the cache build was successful?";    
+		echo "################################################################"
+		echo "Check if the cache build was successful?";
         if fc-list | grep -i $FONT >/dev/null; then
-            echo "################################################################" 
+            echo "################################################################"
             echo "The font was sucessfully installed!";
         else
-        	echo "################################################################" 
+        	echo "################################################################"
             echo "Something went wrong while trying to install the font.";
         fi
     else
-    	echo "################################################################" 	
+    	echo "################################################################"
         echo "Skipping the installation of the font.";
         echo "Please note that this conky configuration will not work";
         echo "correctly without the font.";
     fi
 
-fi
+  fi
+}
+
+#FONT="Inconsolata"
+FONT="Feenacasual"
+#FONT="KnifeFightBallet-Regular"
+install_font $FONT
 
 
-
-##################################################################################################################
-########################                    D E P E N D A N C I E S                     ##########################
-##################################################################################################################
-
-
-
-
-echo "################################################################"
-echo "Checking dependancies"
-
-DISTRO=$(lsb_release -si)
-
-echo "################################################################"
-echo "You are working on " $DISTRO
-echo "################################################################"
-
-
-
-case $DISTRO in 
-
-	LinuxMint|linuxmint|Ubuntu|ubuntu)
-
-
-	# C O N K Y
-
-		# check if conky is installed
-		if ! location="$(type -p "conky")" || [ -z "conky" ]; then
-
-			echo "################################################################"
-			echo "installing conky for this script to work"
-			echo "################################################################"
-
-		  	sudo apt-get install conky-all
-
-		  else
-		  	echo "Conky was installed. Proceeding..."
-		fi
-
-	
-
-		;;
-
-	Arch)
-
-		echo "You are using an arch machine"
-		echo "For this conky to work fully"
-		echo "you need to install the following packages"
-		echo "- conky-lua"
-		
-		;;
-
-	*)
-		echo "No dependancies installed."
-		echo "No installation lines for your system."
-		;;
-esac
-
-##################################################################################################################
 ########################                    S T A R T  O F  C O N K Y                   ##########################
 ##################################################################################################################
 
-echo "################################################################"
 echo "Starting the conky"
-echo "################################################################"
 
-#starting the conky 
-conky -q -c ~/.config/conky/conky.conf
+sh $HOME/.config/conky/start-conky.sh
 
-echo "################################################################"
-echo "###################    T H E   E N D      ######################"
-echo "################################################################"
